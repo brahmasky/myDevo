be2283610e2c8a701f5538198aaa5c5fb82a2b35
brahmasky <paco.luo@gmail.com>
2022-05-29 22:49:13 +1000

    add classify number functions

---
+++ Exercise1/classify_numbers.py
@@ -0,0 +1,63 @@
+import math
+ 
+def proper_divisors(n):
+  '''Return a list of proper divisors of a given number n'''
+  # List to store smaller half of the divisors
+  divisors = []
+  for i in range(1, int(math.sqrt(n) + 1)) :
+    if (n%i==0) :
+      # Check if divisors are equal
+      if (n/i==i) :
+        divisors.append(i)
+      else :
+        # Otherwise add both divisors to the list
+        divisors.extend([i, n//i])
+  return divisors
+
+# keeping this function here for a comparison
+# it's actually slower than using sum of proper divisors 
+def sum_factors(n):
+  '''Returns sum of all factors of a given number n by using prime factors '''
+  res = 1
+  for i in range(2, int(math.sqrt(n) + 1)):
+    curr_sum = 1
+    curr_term = 1
+    while n % i == 0:          
+      n = n / i;
+      curr_term = curr_term * i;
+      curr_sum += curr_term;       
+    res = res * curr_sum  
+  # This condition is to handle the
+  # case when n is a prime number
+  # greater than 2
+  if n > 2:
+      res = res * (1 + n)
+  return res;
+
+def classify_numbers(number_list):
+  '''Classify the number in given list as perfect/deficient/abundant number'''
+  result={}
+  for n in number_list:
+    # sum all proper divisors and minus the given number itself
+    sum_result = sum(proper_divisors(n)) - n
+    if sum_result == n:
+      result[n] = 'perfect'
+    elif sum_result < n:
+      result[n] = 'deficient'
+    else:
+      result[n] = 'abundant'
+  return result
+
+def classify_numbers_with_prime_factor(number_list):
+  '''Classify number using sum_factors function'''
+  result={}
+  for n in number_list:
+    # sum all proper divisors and minus the given number itself
+    sum_result = sum_factors(n) - n
+    if sum_result == n:
+      result[n] = 'perfect'
+    elif sum_result < n:
+      result[n] = 'deficient'
+    else:
+      result[n] = 'abundant'
+  return result
